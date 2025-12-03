19d25d0d31d833b539cdf0573574ba698c37db4e
cmdrumright <cdrumright@protonmail.com>
2025-12-03 11:56:23 -0600

    - added FoodTicketHolder and imported to TicketBooth - added food ticket button and dispatched food ticket event

--- scripts/TicketBooth.js
+++ scripts/TicketBooth.js
@@ -1,13 +1,18 @@
-import { RideTicketHolders } from "./rides/RideTicketHolders.js"
+import {  } from "./rides/RideTicketHolders.js"
+import {  } from "./food/FoodTicketHolders.js"
 
 const contentTarget = document.querySelector(".entry")
 const eventHub = document.querySelector("#state-fair")
+const rideEvent = new CustomEvent("rideTicketPurchased")
+const foodEvent = new CustomEvent("foodTicketPurchased")
 
 eventHub.addEventListener("click", clickEvent => {
     const clickTarget = clickEvent.target
     if (clickTarget.id === "rideTicket" ) {
-        const rideEvent = new CustomEvent("rideTicketPurchased")
         eventHub.dispatchEvent(rideEvent)
+    }
+    if (clickTarget.id === "foodTicket" ) {
+        eventHub.dispatchEvent(foodEvent)
     }
 })
 
@@ -15,8 +20,8 @@ export const TicketBooth = () => {
     contentTarget.innerHTML = `
         <div class="ticketBooth">
             <button id="rideTicket">Ride Ticket</button>
+            <button id="foodTicket">Food Ticket</button>
         </div>
     `
-    RideTicketHolders()
 }
 
---
+++ scripts/food/FoodTicketHolders.js
@@ -0,0 +1,12 @@
+const contentTarget = document.querySelector(".food")
+const eventHub = document.querySelector("#state-fair")
+
+// export const RideTicketHolders = () => {
+//     eventHub.addEventListener("rideTicketPurchased", customEvent => {
+//         contentTarget.innerHTML += `<div class="person rider"></div>`
+//     })
+// }
+
+eventHub.addEventListener("foodTicketPurchased", customEvent => {
+    contentTarget.innerHTML += `<div class="person eater"></div>`
+})
