04916be95abe06b7e68312dcadade78e4e1c1aaa
cmdrumright <cdrumright@protonmail.com>
2025-12-03 09:55:00 -0600

    added custom event listener for adding riders

--- scripts/rides/RideTicketHolders.js
+++ scripts/rides/RideTicketHolders.js
@@ -1,9 +1,12 @@
 const contentTarget = document.querySelector(".rides")
 const eventHub = document.querySelector("#state-fair")
 
-export const RideTicketHolders = () => {
-    eventHub.addEventListener("rideTicketPurchased", customEvent => {
-        contentTarget.innerHTML += `<div class="person rider"></div>`
-    })
-}
+// export const RideTicketHolders = () => {
+//     eventHub.addEventListener("rideTicketPurchased", customEvent => {
+//         contentTarget.innerHTML += `<div class="person rider"></div>`
+//     })
+// }
 
+eventHub.addEventListener("rideTicketPurchased", customEvent => {
+    contentTarget.innerHTML += `<div class="person rider"></div>`
+})
